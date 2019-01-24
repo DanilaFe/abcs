6b3fcf972fc3fe404f3fa7c2a03b5b4f430e4530
Danila Fedorin <danila.fedorin@gmail.com>
2019-01-24 14:42:55 -0800

    Add more tests.

--- tests/refcount_test.cpp
+++ tests/refcount_test.cpp
@@ -44,9 +44,69 @@ greatest_test_res ref_new_not_null() {
     PASS();
 }
 
+greatest_test_res ref_copy_value() {
+    libab_ref a;
+    libab_ref b;
+    int times_freed = 0;
+    auto free_function = [](void* to_free) { int* cast = (int*) to_free; (*cast)++; };
+
+    libab_ref_new(&a, &times_freed, free_function);
+    ASSERT_EQ(times_freed, 0);
+    libab_ref_copy(&a, &b);
+    ASSERT_EQ(times_freed, 0);
+    libab_ref_free(&a);
+    ASSERT_EQ(times_freed, 0);
+    libab_ref_free(&b);
+    ASSERT_EQ(times_freed, 1);
+    PASS();
+}
+greatest_test_res ref_copy_null() {
+    libab_ref a;
+    libab_ref b;
+
+    libab_ref_null(&a);
+    libab_ref_copy(&a, &b);
+    ASSERT_EQ(b.null, 1);
+    libab_ref_free(&a);
+    libab_ref_free(&b);
+    PASS();
+}
+
+greatest_test_res ref_swap() {
+    libab_ref a;
+    libab_ref b;
+    int* first_value = new int;
+    int* second_value = new int;
+    auto free_function = [](void* to_free) { delete (int*) to_free; };
+
+    libab_ref_new(&a, first_value, free_function);
+    libab_ref_new(&b, second_value, free_function);
+    libab_ref_swap(&a, &b);
+    ASSERT_EQ(libab_ref_get(&a), second_value);
+    ASSERT_EQ(libab_ref_get(&b), first_value);
+    libab_ref_free(&a);
+    libab_ref_free(&b);
+    PASS();
+}
+
+greatest_test_res ref_get() {
+    libab_ref ref;
+    int* malloced_value = new int;
+    auto free_function = [](void* to_free) { delete (int*) to_free; };
+
+    libab_ref_new(&ref, malloced_value, free_function);
+    ASSERT_EQ(libab_ref_get(&ref), malloced_value);
+    libab_ref_free(&ref);
+    PASS();
+}
+
 SUITE(refcount_suite) {
     RUN_TEST(ref_new_is_strong);
     RUN_TEST(ref_new_no_gc);
     RUN_TEST(ref_new_free_func);
     RUN_TEST(ref_new_not_null);
+    RUN_TEST(ref_copy_value);
+    RUN_TEST(ref_copy_null);
+    RUN_TEST(ref_swap);
+    RUN_TEST(ref_get);
 }
--- tests/refcount_test.hpp
+++ tests/refcount_test.hpp
@@ -8,4 +8,11 @@ greatest_test_res ref_new_no_gc();
 greatest_test_res ref_new_free_func();
 greatest_test_res ref_new_not_null();
 
+greatest_test_res ref_copy_value();
+greatest_test_res ref_copy_null();
+
+greatest_test_res ref_swap();
+
+greatest_test_res ref_get();
+
 void refcount_suite();
