a99b7bd42a8bb1ee399941e709a9abc22d25f233
Danila Fedorin <danila.fedorin@gmail.com>
2019-01-24 13:36:21 -0800

    Add basic tests to ensure greatest is working.

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,5 +1,16 @@
 #include <cstdlib>
+#include "greatest.h"
 
-int main() {
-    return EXIT_SUCCESS;
+/* Sanity check test */
+TEST one_should_equal_one() {
+    ASSERT_EQm("Reality is broken!", 1, 1);
+    PASS();
+}
+
+GREATEST_MAIN_DEFS();
+
+int main(int argc, char** argv) {
+    GREATEST_MAIN_BEGIN();
+    RUN_TEST(one_should_equal_one);
+    GREATEST_MAIN_END();
 }
